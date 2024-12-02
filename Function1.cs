c6d72292b2a870b25f557b9a3bebbbc9232608d7
nahedkadih <nkadih@yahoo.com>
2024-12-01 20:39:20 -0500

    fuxed

---
+++ Function1.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace FunctionApp1_48
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("This is version 1 NET 4.8");
+
+            return await Task.FromResult (req.CreateResponse(HttpStatusCode.OK, "This is version 1 NET 4.8 ") );
+        }
+    }
+}
